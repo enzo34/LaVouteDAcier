ca05838719ad31c2ed0e48d073ef23a24231e4ae
enzo34 <eclavelloux@gmail.com>
2018-11-03 11:38:17 +0100

    Implement bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= resuire bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
