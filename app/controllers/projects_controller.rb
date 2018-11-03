a1ae04da7efb81d72e03aabb6483caa3f1d4852b
enzo34 <eclavelloux@gmail.com>
2018-11-03 10:43:21 +0100

    Fisinish the CRUD

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,12 @@ class ProjectsController < ApplicationController
   before_action :find_project, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Project.all
+    if params[:category].blank?
+      @projects = Project.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @projects = Project.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
